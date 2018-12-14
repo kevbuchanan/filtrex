cb74bb5c1b78203141b8fa631a6efe62d6f90964
Kevin Buchanan <kevaustinbuch@gmail.com>
2018-12-14 13:56:05 -0600

    Upgrade to Ecto 3

--- mix.exs
+++ mix.exs
@@ -2,25 +2,26 @@ defmodule Filtrex.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :filtrex,
-     version: "0.4.2",
-     elixir: "~> 1.3",
-     description: description(),
-     package: package(),
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     elixirc_paths: elixirc_paths(Mix.env),
-     deps: deps(),
-     name: "Filtrex",
-     docs: [main: "Filtrex",
-            source_url: "https://github.com/rcdilorenzo/filtrex"]]
+    [
+      app: :filtrex,
+      version: "0.4.2",
+      elixir: "~> 1.3",
+      description: description(),
+      package: package(), build_embedded: Mix.env() == :prod,
+      start_permanent: Mix.env() == :prod,
+      elixirc_paths: elixirc_paths(Mix.env()),
+      deps: deps(),
+      aliases: aliases(),
+      name: "Filtrex",
+      docs: [main: "Filtrex", source_url: "https://github.com/rcdilorenzo/filtrex"]
+    ]
   end
 
   defp elixirc_paths(:test), do: ~w(lib test/support)
   defp elixirc_paths(_), do: ~w(lib)
 
   def application do
-    [applications: [:logger, :tzdata] ++ applications(Mix.env)]
+    [applications: [:logger, :tzdata] ++ applications(Mix.env())]
   end
 
   defp applications(:test), do: [:postgrex, :ecto, :ex_machina]
@@ -34,12 +35,13 @@ defmodule Filtrex.Mixfile do
 
   defp deps do
     [
-      {:postgrex, ">= 0.0.0", only: :test},
-      {:ecto, "~> 2.1"},
+      {:ecto, "~> 3.0"},
+      {:ecto_sql, "~> 3.0"},
       {:timex, "~> 3.1"},
       {:earmark, "~> 0.1", only: :dev},
       {:ex_doc, "~> 0.11", only: :dev},
       {:inch_ex, ">= 0.0.0", only: [:dev, :docs]},
+      {:postgrex, ">= 0.0.0", only: :test},
       {:plug, "~> 1.1.2", only: :test},
       {:ex_machina, "~> 0.6.1", only: :test},
       {:mix_test_watch, "~> 0.3", only: :dev, runtime: false}
@@ -50,8 +52,16 @@ defmodule Filtrex.Mixfile do
     [
       maintainers: ["Christian Di Lorenzo"],
       licenses: ["MIT"],
-      links: %{"GitHub" => "https://github.com/rcdilorenzo/filtrex",
-               "Docs" => "http://rcdilorenzo.github.io/filtrex"}
+      links: %{
+        "GitHub" => "https://github.com/rcdilorenzo/filtrex",
+        "Docs" => "http://rcdilorenzo.github.io/filtrex"
+      }
+    ]
+  end
+
+  defp aliases do
+    [
+      test: ["ecto.create --quiet", "ecto.migrate", "test"]
     ]
   end
 end
